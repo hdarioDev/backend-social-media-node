0d175289a3a30c10a7498909ce806170ef25babf
hdariodev <hdariodev@gmail.com>
2023-03-15 16:53:45 -0400

    integration vercel

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,12 @@
 const ctrl = require("./controller");
 // const store = require("../../../store/mysql");
-const store = require("../../../store/remote-mysql");
+// const store = require("../../../store/remote-mysql");
+
+let store;
+if (config.remoteDB === true) {
+  store = require("../../../store/remote-mysql");
+} else {
+  store = require("../../../store/mysql");
+}
 
 module.exports = ctrl(store);
